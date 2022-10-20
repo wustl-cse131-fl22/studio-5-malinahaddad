57c06fd6cbc1b0988d071431601236a7c894a448
Dylan <Dylan@DESKTOP-5OLPTT0.dhcp.wustl.edu>
2022-10-20 12:45:22 -0500

    studio done

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -14,7 +14,7 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
+		double distance = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow(y2-y1, 2));
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
 		return distance;
@@ -34,16 +34,22 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
+		StdDraw.setPenColor(0, 109, 219);
+		StdDraw.filledCircle(x, y, radius*(3.0/4.0));
 
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
+		StdDraw.setPenColor(146, 0, 0);
+		StdDraw.filledCircle(x, y, radius*0.5);
 
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
+		StdDraw.setPenColor(255, 255, 109);
+		StdDraw.filledCircle(x, y, radius/4.0);
 
 		
 	}
@@ -61,6 +67,11 @@ public class Methods {
 	 */
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
+		char [] word = source.toCharArray();
+		for (int i=0; i<word.length; i++) {
+			char newChar = word[i];
+			result 
+		}
 		// TODO: Finish this method
 		
 		return result;
